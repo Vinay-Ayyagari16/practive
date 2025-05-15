cc3f538f4d070e45cd87e44b5242fe76b74172b2
Vinay <vinayayyagari6@gmail.com>
2025-05-15 18:04:47 +0530

    concle

---
+++ list.py
@@ -0,0 +1,10 @@
+squares = [x**2 for x in range(5)]
+print(squares)  
+
+nums = [1, 2, 3, 4, 5, 6]
+evens = [x for x in nums if x % 2 == 0]
+print(evens)  
+
+words = ['hello', 'world']
+upper_words = [word.upper() for word in words]
+print(upper_words)  
